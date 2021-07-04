2545b0aa542954eaed3c22d3d6ec1c749193f013
yucheng <yucheng2k13@gmail.com>
2021-07-04 16:44:43 +0800

    spring data jpa

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that is it ... no need to write any code LOL!!
+}
